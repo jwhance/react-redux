e0bcc6d868f6d2a3543e2b39074809b3372a2d9c
Hance <Joseph.Hance@salliemae.com>
2021-01-28 07:31:17 -0500

    Added 2nd data source

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,23 +1,32 @@
 import React from "react";
 import { connect } from 'react-redux';
 import * as courseActions from '../../redux/actions/courseActions';
+import * as authorActions from '../../redux/actions/authorActions';
+
 import PropTypes from 'prop-types';
 import {bindActionCreators} from 'redux';
+import CourseList from './CourseList';
 
 class CoursesPage extends React.Component {
     componentDidMount() {
-        this.props.actions.loadCourses().catch(error => {
-            alert('Loading courses failed ' + error);
-        })
+        if (this.props.courses.length === 0) {
+            this.props.actions.loadCourses().catch(error => {
+                alert('Loading courses failed ' + error);
+            });
+        }
+
+        if (this.props.authors.length === 0){
+            this.props.actions.loadAuthors().catch(error => {
+                alert('Loading authors failed ' + error);
+            });        
+        }
     }
 
     render() {
         return (
             <>
                 <h2>Courses</h2>
-                {this.props.courses.map(course => (
-                    <div key={course.title}>{course.title}</div>
-                ))}
+                <CourseList courses={this.props.courses} />
             </>
         );
     }
@@ -25,18 +34,29 @@ class CoursesPage extends React.Component {
 
 CoursesPage.propTypes = {
     actions: PropTypes.object.isRequired,
-    courses: PropTypes.array.isRequired
+    courses: PropTypes.array.isRequired,
+    authors: PropTypes.array.isRequired
 };
 
 function mapStateToProps(state){
     return {
-        courses: state.courses      // Be SPECIFIC.  Request only the data your component needs
+        courses: 
+            state.authors.length === 0 ? [] : state.courses.map(course => {
+            return {
+                ...course,
+                authorName: state.authors.find(a => a.id === course.authorId).name
+            };
+        }),
+        authors: state.authors
     };
 }
 
 function mapDispatchToProps(dispatch){
     return {
-        actions: bindActionCreators(courseActions, dispatch)
+        actions: {
+            loadCourses: bindActionCreators(courseActions.loadCourses, dispatch),
+            loadAuthors: bindActionCreators(authorActions.loadAuthors, dispatch)
+        }
     };
 }
 
--- src/redux/reducers/courseReducer.js
+++ src/redux/reducers/courseReducer.js
@@ -1,13 +1,14 @@
 import * as types from '../actions/actionTypes';
+import initialState from './initialState';
 
-const courseReducer = (state = [], action) => {
+const courseReducer = (state = initialState.courses, action) => {
     switch(action.type) {
         case types.CREATE_COURSE:
             return [...state, {...action.course}];
 
         case types.LOAD_COURSES_SUCCESS:
             return action.courses;
-            
+
         default:
             return state;
     }
